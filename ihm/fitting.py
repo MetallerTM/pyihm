Core of the fit done, but wrong results

ihm/fitting.py
@@ -7,18 +7,23 @@ from gen_param import L2P
 import lmfit as l
 
 # Get the parameters from the other function
-acqus, Lparam, param = gen_param()
+M, acqus, Lparam, param = gen_param()
 
 # I put this because I didn't want to read it
+N = M.r.shape[-1]
 N_spectra = 2
+exp = np.copy(M.r)
+I = processing.integral(exp) * (2 * M.acqus['dw']) / 2
 
-@cron
-def f2min(Lparam, param, N_spectra, acqus):
+def calc_spectra(Lparam, param, N_spectra, acqus, N, use_L=True):
     # Convert normalized parameters into new parameters
-    for key in Lparam:
-        newkey = key.replace('L', '', 1)
-        value = L2P(Lparam[key].value, param[newkey].min, param[newkey].max)
-        param[newkey].set(value)
+    if use_L:
+        for key in Lparam:
+            if key == 'count':
+                continue
+            newkey = key.replace('L', '', 1)
+            value = L2P(Lparam[key].value, param[newkey].min, param[newkey].max)
+            param[newkey].set(value)
 
     # Separate the parameters according to the spectra
     d_param = param.valuesdict()    # Convert to dictionary
@@ -48,17 +53,79 @@ def f2min(Lparam, param, N_spectra, acqus):
             k=dic[f'k{i}'],
             x_g=0, 
             phi=0,
-            N=None,
+            N=N,
             ) for i in peaks_idx[n]]
         # Compute the trace for each peak, then sum them up, finally multiply by the intensity
         spectra.append(dic['I'] * np.sum([peak() for peak in peak_list], axis=0))
+    return spectra
+
+#@cron
+def f2min(Lparam, param, N_spectra, acqus, N, exp, I):
+    Lparam['count'].set(Lparam['count'].value + 1)
+    spectra = calc_spectra(Lparam, param, N_spectra, acqus, N)
 
     # Sum the spectra to give the total fitting trace
     total = np.sum(spectra, axis=0)
-    return total
+    residual = exp - I * total
+    if Lparam['count'].value % 500 == 0: 
+        print([f'{key}, {param[key].value}' for key in param if 'I' in key ])
+        fig = plt.figure()
+        plt.plot(exp, label='E')
+        for k, s in enumerate(spectra):
+            plt.plot(s, label=f'c{k+1}')
+        plt.plot(total, label='F')
+        plt.plot(residual, '--', label='R')
+        plt.ylim(-max(exp), max(exp))
+        plt.legend()
+        plt.savefig(f'fit_{Lparam["count"].value}.tiff', dpi=600)
+        plt.close()
+    return np.sum(residual**2)
 
-total = f2min(Lparam, param, 2, acqus)
-plt.plot(total)
+i_spectra = calc_spectra(Lparam, param, N_spectra, acqus, N)
+
+plt.figure()
+plt.plot(M.ppm, exp, lw=3)
+plt.plot(M.ppm, I * np.sum(i_spectra, axis=0))
+plt.figure()
+plt.plot(M.ppm, exp, lw=3)
+for s in i_spectra:
+    plt.plot(M.ppm, I * s)
+plt.figure()
+plt.plot(M.ppm, exp - np.sum(i_spectra, axis=0) * I, '--')
+plt.show()
+
+param.pretty_print()
+print('='*96)
+
+count = 0
+Lparam.add('count', value=0, vary=False)
+
+minner = l.Minimizer(f2min, Lparam, fcn_args=(param, N_spectra, acqus, N, exp, I))
+result = minner.minimize(method='nelder', max_nfev=10000, tol=1e-10)
+#print(result.message, result.nfev)
+print('')
+
+param.pretty_print()
+print('='*96)
+
+Lopt = result.params
+
+opt_spectra = calc_spectra(Lopt, param, N_spectra, acqus, N)
+
+K = [f for key, f in param.valuesdict().items() if 'I' in key]
+K_norm, _ = misc.molfrac(K)
+print(K_norm)
+
+
+plt.figure()
+plt.plot(exp, lw=3)
+plt.plot(I * np.sum(opt_spectra, axis=0))
+plt.figure()
+plt.plot(exp, lw=3)
+for s in opt_spectra:
+    plt.plot(I * s)
+plt.figure()
+plt.plot(exp - np.sum(opt_spectra, axis=0) * I, '--')
 plt.show()
 
 

ihm/spectra_reading.py
@@ -32,6 +32,9 @@ class Multiplet:
         self.peaks = {}
         for peak in peaks:
             self.peaks[peak.idx] = peak
+            self.N = peak.N
+            if self.N is None:
+                self.N = int(self.acqus['TD'])
 
         # Compute the mean chemical shift and the offsets
         self.U = np.mean([p.u for _, p in self.peaks.items()])
@@ -67,7 +70,7 @@ class Multiplet:
         - trace: 1darray
             Sum of the components
         """
-        trace = np.zeros(self.acqus['TD'])  # Placeholder
+        trace = np.zeros(self.N)  # Placeholder
         for key, peak in self.peaks.items():
             # Recompute the absolute chemical shift
             self.peaks[key].u = self.U + self.u_off[key]
@@ -108,6 +111,9 @@ class Spectr:
         self.peaks = {}
         for peak in peaks:
             self.peaks[peak.idx] = peak
+            self.N = peak.N
+            if self.N is None:
+                self.N = int(self.acqus['TD'])
 
         ## Sort the peaks according to the 'group' attribute: this separates the multiplets
         all_groups = {key: p.group for key, p in self.peaks.items()}    # Get the group labels
@@ -119,7 +125,7 @@ class Spectr:
             # Get only the peaks of the same group
             keys = [key for key, item in all_groups.items() if item == g]
             if g == 0:  # They are independent, treated as singlets
-                self.p_collections[0] = [fit.Peak(self.acqus, **self.peaks[key].par()) for key in keys]
+                self.p_collections[0] = [fit.Peak(self.acqus, N=self.N, **self.peaks[key].par()) for key in keys]
                 # Add the labels as 'idx' attributes
                 for k, key in enumerate(keys):
                     self.p_collections[0][k].idx = key 
@@ -137,7 +143,7 @@ class Spectr:
         - total: 1darray
             Computed spectrum
         """
-        total = np.zeros(self.acqus['TD'])  # Placeholder
+        total = np.zeros(self.N)  # Placeholder
         for g in self.unique_groups:
             if g == 0:  # Group 0 is a list of peaks!
                 for s in self.p_collections[g]:
@@ -163,11 +169,18 @@ class Spectr:
 
 
 def main():
-
     # Load the acqus dictionary of the (mixture) spectrum
-    acqus = sim.load_sim_1D('Sp_1.acqus')
+    M = Spectrum_1D('M.acqus', isexp=False)
+    M.fid = 7 * np.loadtxt('M.fid', dtype='complex128')
+    M.procs['zf'] = 16384
+    M.process()
+    M.plot()
+
+    acqus = M.acqus
+    N = M.r.shape[-1]
+
     # Create list of peaks files
-    spectra_dir = ['Sp_1.fvf', 'Sp_1.fvf']
+    spectra_dir = ['C_1.fvf', 'C_2.fvf']
 
 
     ## Gather all the peaks
@@ -184,8 +197,7 @@ def main():
             for key in sorted(region_peaks.keys()): # Iterate on the peak index
                 p = dict(region_peaks[key]) # Alias, shortcut
                 # Create the fit.Peak object and append it to the peaks list. Use the ABSOLUTE intensities in order to not mess up with different windows!
-                # TODO cambiare N per matchare le dimensioni dello spettro della miscela
-                peaks.append(fit.Peak(acqus, u=p['u'], fwhm=p['fwhm'], k=I*p['k'], x_g=p['x_g'], phi=0, N=None, group=p['group']))
+                peaks.append(fit.Peak(acqus, u=p['u'], fwhm=p['fwhm'], k=I*p['k'], x_g=p['x_g'], phi=0, N=N, group=p['group']))
                 # Add the peak index as "floating" attribute
                 peaks[-1].idx = key
             # Once all the peaks in a given region have been generated, store them in the list 
@@ -203,6 +215,4 @@ def main():
         # At the end, generate the Spectr object and add it to a list
         components.append(Spectr(acqus, *whole_spectrum))
 
-        
-
-    return acqus, components
+    return M, acqus, components