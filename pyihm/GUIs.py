Added new comodity features. Removed debugging prints. Modified output precision. Bug fixes.

pyihm/GUIs.py
@@ -202,7 +202,7 @@ def select_regions(ppm_scale, spectrum, full_calc):
     return regions
 
 
-def cal_gui(exp, ppm_scale, components, I, prev_Icorr=None, rav_flag=False):
+def cal_gui(exp, ppm_scale, components, I, prev_Icorr=None, init_active=1, rav_flag=False):
     """
     Corrects the chemical shifts and the intensities of the spectra to be employed during the fit.
     Works together with edit_gui, that allows to break up a spectrum in single components in order to adjust them.
@@ -216,6 +216,12 @@ def cal_gui(exp, ppm_scale, components, I, prev_Icorr=None, rav_flag=False):
         Spectra to calibrate
     - I: float
         Intensity correction for the experimental spectrum
+    - prev_Icorr: list
+        Starting concentration to be further edited
+    - init_active: int
+        Initial spectrum to be activated
+    - rav_flag: bool
+        Activates colorblind paelette
     ------------
     Returns:
     - exit_code: int
@@ -224,7 +230,6 @@ def cal_gui(exp, ppm_scale, components, I, prev_Icorr=None, rav_flag=False):
         Correction for the chemical shift correction of each spectrum, in ppm
     - Icorr
         Correction for the intensity of each spectrum
-    return exit_code, drifts, Icorr
     """
     # Initialize variables and resets
     if rav_flag:
@@ -275,7 +280,7 @@ def cal_gui(exp, ppm_scale, components, I, prev_Icorr=None, rav_flag=False):
 
     ## WIDGETS
     #   Selector slider
-    slider = Slider(box_slider, 'Spectrum', valmin=1, valmax=N_spectra, valinit=1, valstep=1, orientation='vertical')
+    slider = Slider(box_slider, 'Spectrum', valmin=1, valmax=N_spectra, valinit=init_active, valstep=1, orientation='vertical')
     #   Radiobuttons
     radio = RadioButtons(box_radio, ['DRIFT', 'INTENS'])
     #   Sensitivity buttons
@@ -449,9 +454,9 @@ def cal_gui(exp, ppm_scale, components, I, prev_Icorr=None, rav_flag=False):
         line, = ax.plot(ppm_scale, I_s[k]*y, c=colors['nonact'], lw=0.8)
         y_lines.append(line)
     # First one set as active
-    y_lines[0].set_color(colors['act'])
-    y_lines[0].set_lw(2)
-    y_lines[0].set_zorder(10)
+    y_lines[init_active-1].set_color(colors['act'])
+    y_lines[init_active-1].set_lw(2)
+    y_lines[init_active-1].set_zorder(10)
     total_line, = ax.plot(ppm_scale, np.sum([i*y for i, y in zip(I_s, components)], axis=0), c=colors['total'], lw=0.4, zorder=2)
     total_line.set_visible(False)
 

pyihm/fit_mixture.py
@@ -356,12 +356,12 @@ def write_output(M, I, K, spectra, n_comp, lims, filename='fit.report'):
     f = open(filename, 'w', buffering=1)
 
     ## HEADER
-    f.write('Fit performed by {} on {}\n\n'.format(os.getlogin(), date_and_time))
+    f.write('! Fit performed by {} on {}\n\n'.format(os.getlogin(), date_and_time))
     f.write(f'Mixture spectrum: {os.path.join(M.datadir, M.filename)}\n\n')
     f.write(f'Absolute intensity correction: I = {I:.5e}\n\n')
     f.write('Relative intensities:\n')
     for k, r_i in enumerate(K):
-        f.write(f'Comp. {n_comp[k]:>3}: {r_i*100:10.5g}%\n')
+        f.write(f'Component {n_comp[k]:>3.0f}: {r_i*100:10.5f}% | Rel: {r_i/min(K):10.5f}\n')
     f.write('\n\n\n')
     f.close()
 
@@ -578,47 +578,27 @@ def main(M, N_spectra, Hs, param, I, lims=None, fit_kws={}, filename='fit', NOAL
     Hf = np.empty(len(Hs))
     for k, peaks in enumerate(opt_spectra_obj):
         Hf[k] = np.sum([peak.k for peak in peaks])
-    print('Hf', Hf)
     #   ... and finally make the total trace
     opt_total = np.sum(opt_spectra, axis=0)
 
     component_idx = [int(key.split('_')[0].replace('S', '')) for key in popt if 'I' in key]
-    if 0:
-        # Normalize the intensities so that they sum up to 1
-        for k, n in enumerate(component_idx):
-            In = popt[f'S{n}_I'].value    # Intensity of the n-th spectrum from the fit
-            # Get relative intensities of the components of the n-th spectrum
-            ri_dict = {key: f for key, f in popt.valuesdict().items() if f'S{n}' in key and 'k' in key}
-            ri = [f for key, f in ri_dict.items()]
-            # Normalize them
-            ri_norm, In_corr = kz.misc.molfrac(ri)
-            # Correct the intensity of the n-th spectrum
-            popt[f'S{n}_I'].set(value=In*In_corr/Hs[k])
-            # Update the parameters
-            for key, value in zip(ri_dict.keys(), ri_norm):
-                popt[key].set(value=value*Hs[k])
 
     #   Get the actual intensities
     concentrations = np.array([f for key, f in popt.valuesdict().items() if 'I' in key])
 
     #   Normalize them
     c_norm, I_corr = kz.misc.molfrac(concentrations)
-    print('Kn', c_norm)
-    print('Ic', I_corr)
     #   Correct the total intensity to preserve the absolute values
     I_abs = I * I_corr
-
-    # Calculate the concentration of the components 
-    #I_mixture, _ = kz.misc.molfrac(np.array(K_norm))# / np.array(Hs))
 
     # Print relative concentrations of the components
     print('FIT RESULTS')
     for k, K in enumerate(c_norm):
-        print(f'Component {component_idx[k]:2.0f}: {K*100:10.5g}% | Rel: {K/min(c_norm):10.3f}')
+        print(f'Component {component_idx[k]:2.0f}: {K*100:10.5f}% | Rel: {K/min(c_norm):10.5f}')
     print()
     
     # Write the output
-    write_output(M, I_abs, concentrations, opt_spectra_obj, 
+    write_output(M, I_abs, c_norm, opt_spectra_obj, 
             n_comp = component_idx,
             lims=(np.max(np.array(lims)), np.min(np.array(lims))), 
             filename=f'{filename}.out')

pyihm/spectra_reading.py
@@ -245,7 +245,7 @@ def get_component_spectrum(filename, acqus, return_dict=True, N=None, norm=True)
         return total, I
 
 
-def main(M, spectra_dir, Hs, lims=None, cal_flag=False, rav_flag=False):
+def main(M, spectra_dir, Hs, lims=None, I0=None, cal_flag=False, rav_flag=False):
     """
     Reads the .fvf files, containing the fitted parameters of the peaks of a series of spectra.
     Then, computes a list of Spectr objects with those parameters, and returns it.
@@ -261,13 +261,14 @@ def main(M, spectra_dir, Hs, lims=None, cal_flag=False, rav_flag=False):
         Number of protons each spectrum integrates for
     - lims: list of tuple
         Borders of the fitting windows, in ppm (left, right)
+    - I0: list
+        Initial guess for the concentrations
     - cal_flag: bool
         If True, opens the optimization of the initial guess through GUI
     - rav_flag: bool
         If True, the GUIs will be drawn in colorblind palette
     ----------
     Returns:
-    return components, Hs, I0, lims, c_idx, I
     - components: list of Spectr
         Spectra to be used as model in the fit. Contain only the peaks within the fittin regions.
     - Hs: list
@@ -324,16 +325,19 @@ def main(M, spectra_dir, Hs, lims=None, cal_flag=False, rav_flag=False):
     # List of 1darrays (one per spectrum)
     components = [get_component_spectrum(file, acqus, False, norm=Hs[k], N=M.r.shape[-1])[0] for k, file in enumerate(spectra_dir)]
 
-    exit_code = True    # Placeholder
+    exit_code = 1    # Placeholder
     # Compute intensity
     I = kz.processing.integrate(M.r, x=M.freq) / (acqus['SW']/2) /np.sum(Hs)
     # Compute initial guess for concentrations
-    Icorr = [1. for k in range(len(components))]
+    if I0:
+        Icorr = [C for C in I0]
+    else:
+        Icorr = [1. for k in range(len(components))]
 
     if cal_flag:
         while exit_code:        # Loop over two functions
             # Try to perform drift and intensity adjustments
-            exit_code, drifts, Icorr = GUIs.cal_gui(M.r, M.ppm, components, I, Icorr, rav_flag)
+            exit_code, drifts, Icorr = GUIs.cal_gui(M.r, M.ppm, components, I, Icorr, exit_code, rav_flag)
             # Apply drift corrections to the peaks
             for k, ucorr in enumerate(drifts):
                 for _, peak in comp_peaks[k].items():
@@ -372,11 +376,15 @@ def main(M, spectra_dir, Hs, lims=None, cal_flag=False, rav_flag=False):
             new_filename = base_name + '-cal.' + extension
             print(filename, new_filename)
             # Write the new fvf file
-            kz.fit.write_vf(new_filename, comp_peaks[k], tmp_lims, 1)#Hs[k])
+            kz.fit.write_vf(new_filename, comp_peaks[k], tmp_lims, 1, header=True)#Hs[k])
         if filename == new_filename:
             print('Calibrated version of the .fvf files updated.')
         else:
             print('Calibrated version of the .fvf files saved.')
+        I0str = ', '.join([f'{C:.4f}' for C in Icorr])
+        print('\nInitial guess for the concentration computed.')
+        print('Add the following lines to the input file:')
+        print(f'I0\n{I0str}\n')
 
     # Select regions for performing the fit
     if lims is None: